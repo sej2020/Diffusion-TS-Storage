c434f42d09f887b1f6e8a2f6a6db255451c56454
sej2020 <sejohnson2020@outlook.com>
2025-02-20 20:09:33 -0500

    example_model + archiving

--- scripts/feat_select_cosine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-feature_cnt=(1 2 4 8 16 32) 
-
-for i in {1..10}; do
-    for f in ${feature_cnt[@]}; do
-        python -m exe_forecasting --nsample 20 --device 'cuda:1' --n_condit_features $f --condit_strat "cosine" --out_folder "feat_select_cosine/n_feat_${f}" --train --eval
-    done
-done
