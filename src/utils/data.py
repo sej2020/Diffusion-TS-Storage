3ed5bbe2681bf574dae4e1cda69d41dcbffa9aea
sej2020 <sejohnson2020@outlook.com>
2025-04-01 14:13:39 -0400

    new feature retention strat

--- src/utils/data.py
+++ src/utils/data.py
@@ -6,6 +6,7 @@ import torch
 from sklearn.decomposition import PCA
 import os
 import math
+import concurrent.futures
 
 from src.utils.utils import data_csv_to_pkl, gen_mask
 
@@ -18,12 +19,13 @@ class ConditionalDataset(Dataset):
         compression,
         feature_retention_strategy,
         history_block_size,
-        model_param_proportion,
+        data_to_model_ratio,
         history_to_feature_ratio,
         window_length,
         training_feature_sample_size,
         data_dayfirst = False,
         train = True,
+        **kwargs
     ):
         self.dataset = dataset
         self.device = device
@@ -31,7 +33,7 @@ class ConditionalDataset(Dataset):
         self.compression = compression
         self.feature_retention_strategy = feature_retention_strategy
         self.history_block_size = history_block_size
-        self.model_param_proportion = model_param_proportion
+        self.data_to_model_ratio = data_to_model_ratio
         self.history_to_feature_ratio = history_to_feature_ratio
         self.window_length = window_length
         self.training_feature_sample_size = training_feature_sample_size
@@ -52,8 +54,11 @@ class ConditionalDataset(Dataset):
         T, N = self.main_data.shape
         n_total_points = T * N
         n_compressed_points = int(n_total_points * self.compression)
-        # We want to keep a certain proportion of the data as model parameters: n_model_points = int(n_compressed_points * model_param_proportion)
-        self.n_model_points = 616033 # but for right now, it is just the base model, which has about 600k parameters
+        ### We want to keep a certain proportion of the data as model parameters: 
+        ###   data_point_percnt = data_to_model_ratio / data_to_model_ratio + 1
+        ###   n_model_points = int(n_compressed_points * (1 - data_point_percnt))
+        ### but for right now, it is just the base model, which has about 600k parameters
+        self.n_model_points = 616033 
         assert self.n_model_points < n_compressed_points, "Model parameters are more than the compressed data, please make your model smaller"
         exp_n_condit_points = n_compressed_points - self.n_model_points
         exp_point_compression = exp_n_condit_points / n_total_points
@@ -63,16 +68,32 @@ class ConditionalDataset(Dataset):
 
         # finding gap sizes
         n_gap_slices = T - self.n_history_slices
-        n_gaps = self.n_history_slices // self.history_block_size
-        history_block_gap = math.ceil(n_gap_slices / n_gaps)
-
+        n_gaps = self.n_history_slices // self.history_block_size # should be the same as number of history blocks
+        history_block_gap = math.ceil(n_gap_slices / n_gaps) # conservative size (bigger than strictly necessary)
 
         # finding features retained idx
-        if self.feature_retention_strategy == 'pca loadings':
-            # reverse order
-            self.condit_feature_idx = pca_loading_selection(self.main_data, self.n_feature_slices)[::-1]
-        else:
-            raise NotImplementedError
+        most_useful = pca_sorted_features(self.main_data)
+        if self.feature_retention_strategy == 'pca':
+            self.condit_feature_idx = most_useful[:self.n_feature_slices]
+        elif self.feature_retention_strategy == 'select':
+
+            if 'selected_features' not in kwargs:
+                raise ValueError("Please provide selected features to retain")
+
+            with open(f'data/{dataset}/labels.pkl', 'rb') as f:
+                # two dictionaries: {'start': int, 'interval': int} and {str: int}
+                (_, var_names) = pickle.load(f)
+            
+            # if len(selected_features) > self.n_feature_slices, then it will be truncated to n_feature_slices
+            selected_feature_idxs = [var_names[v] for v in kwargs['selected_features']][:self.n_feature_slices]
+            # but if not, we fill the rest with the most useful features
+            useful_idx = 0
+
+            while len(selected_feature_idxs) < self.n_feature_slices:
+                if most_useful[useful_idx] not in selected_feature_idxs:
+                    selected_feature_idxs.append(most_useful[useful_idx])
+                useful_idx += 1
+            self.condit_feature_idx = selected_feature_idxs
             
         mask_metadata = {
             'condit_feature_idx': self.condit_feature_idx, 
@@ -92,12 +113,26 @@ class ConditionalDataset(Dataset):
             self.use_index = non_overlapping_index
 
 
+    async def _load_data(self, start_idx, end_idx, feature_idxs):
+        # This function is a placeholder for loading data from a database
+        # In the future, this function should be implemented to load data asynchronously
+        pass
+
+
     def __getitem__(self, orgindex):
+
+        # right now: loads whole dataset into memory and indexs into it
+        # in the future: load data from db and prefetch next index
 
         t_index = self.use_index[orgindex]
 
         if self.train and self.main_data.shape[1] > self.training_feature_sample_size:
             # sample features
+            # if self.prefetched_data index matches t_index, use it
+            # if self.prefetched_data.done() and self.prefetched_data.index == t_index:
+            #     s = self.prefetched_data.result()
+            #     self.prefetched_data = None
+            # else, load data from db
             rand_feature_idxs = np.random.choice(self.main_data.shape[1], self.training_feature_sample_size, replace=False)
             s = {
                 'observed_data': self.main_data[t_index:t_index+self.window_length][:, rand_feature_idxs], # will be db query in future
@@ -110,8 +145,14 @@ class ConditionalDataset(Dataset):
                 'timepoints': np.arange(self.window_length, dtype=np.int32), 
                 'feature_id': rand_feature_idxs, 
             }
+            # executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+            # next_rand_feature_idxs = np.random.choice(self.main_data.shape[1], self.training_feature_sample_size, replace=False)
+            # self.prefetched_data = executor.submit(self._load_data, t_index+1, t_index+self.window_length+1, next_rand_feature_idxs)
+
         else:
             # all features
+            # if self.prefetched_data index matches t_index, use it
+            # else, load data from db
             s = {
                 'observed_data': self.main_data[t_index:t_index+self.window_length], # will be db query in future
                 'presence_mask': gen_mask(
@@ -123,6 +164,8 @@ class ConditionalDataset(Dataset):
                 'timepoints': np.arange(self.window_length, dtype=np.int32), 
                 'feature_id': np.arange(self.main_data.shape[1], dtype=np.int32), 
             }
+            # executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+            # self.prefetched_data = executor.submit(self._load_data, t_index+self.window_length, t_index+self.window_length*2, np.arange(self.main_data.shape[1]))
 
         return s
     
@@ -131,7 +174,7 @@ class ConditionalDataset(Dataset):
         return len(self.use_index)
 
 
-    def _calc_slice_counts(self, point_compression):
+    def _calc_slice_counts(self, point_compression: float):
         T = self.main_data.shape[0]
         N = self.main_data.shape[1]
         z = self.history_to_feature_ratio
@@ -147,7 +190,7 @@ class ConditionalDataset(Dataset):
         if not math.isclose(self.n_condit_points, T*N*C, rel_tol=.01):
             actual_compression = (self.n_condit_points + self.n_model_points) / (T*N)
             print("Warning compression is off by more than 1%")
-            print(f"Expected compression: {self.compression},\nActual: {round(actual_compression, 4)}")
+            print(f"Expected compression: {self.compression}\nActual: {round(actual_compression, 4)}")
 
 
 def get_dataloader(
@@ -157,11 +200,12 @@ def get_dataloader(
     compression,
     feature_retention_strategy,
     history_block_size,
-    model_param_proportion,
+    data_to_model_ratio,
     history_to_feature_ratio,
     window_length,
     training_feature_sample_size,
     data_dayfirst = False,
+    **kwargs
     ):
 
     conditional_dataset_train = ConditionalDataset(
@@ -171,11 +215,12 @@ def get_dataloader(
         compression = compression,
         feature_retention_strategy = feature_retention_strategy,
         history_block_size = history_block_size,
-        model_param_proportion = model_param_proportion,
+        data_to_model_ratio = data_to_model_ratio,
         history_to_feature_ratio = history_to_feature_ratio,
         window_length = window_length,
         training_feature_sample_size = training_feature_sample_size,
         data_dayfirst = data_dayfirst,
+        **kwargs
         )
     
     conditional_dataset_eval = ConditionalDataset(
@@ -185,22 +230,24 @@ def get_dataloader(
         compression = compression,
         feature_retention_strategy = feature_retention_strategy,
         history_block_size = history_block_size,
-        model_param_proportion = model_param_proportion,
+        data_to_model_ratio = data_to_model_ratio,
         history_to_feature_ratio = history_to_feature_ratio,
         window_length = window_length,
         training_feature_sample_size = training_feature_sample_size,
         data_dayfirst = data_dayfirst,
         train = False,
+        **kwargs
         )
     
     scaler = torch.from_numpy(conditional_dataset_train.std_data).to(device).float()
     mean_scaler = torch.from_numpy(conditional_dataset_train.mean_data).to(device).float()
 
-    with open(f"config/train_config.yaml", 'r') as f:
-        config = yaml.safe_load(f)
+    # with open(f"config/train_config.yaml", 'r') as f:
+        # config = yaml.safe_load(f)
 
     train_loader = DataLoader(
-        conditional_dataset_train, batch_size=config['train']['batch_size'], shuffle=1)
+        conditional_dataset_train, batch_size=16, shuffle=1)
+        # conditional_dataset_train, batch_size=config['train']['batch_size'], shuffle=1)
 
     eval_loader = DataLoader(
         conditional_dataset_eval, batch_size=1, shuffle=0)
@@ -209,9 +256,19 @@ def get_dataloader(
 
 
 
-def pca_loading_selection(data, n_features):
+def pca_sorted_features(data: np.ndarray) -> list:
+    """
+    Runs PCA on the data and returns the indices of the features sorted by their absolute loadings. This roughly
+    corresponds to the importance of the features in the PCA model. The features are sorted from most important to least important.
+
+    Args:
+        data (np.ndarray): The data to run PCA on. Should be a 2D array with shape (n_samples, n_features).
+    
+    Returns:
+        list: A list of indices of the features sorted by their absolute loadings.
+    """
     print('\n$$$ PCA $$$\n')
     pca = PCA(n_components=1)
     pca.fit(data)
-    pca_sorted_features = np.argsort(np.abs(pca.components_))[0].tolist()
-    return pca_sorted_features[-n_features:]
+    sorted_features = np.argsort(np.abs(pca.components_))[0].tolist()
+    return sorted_features[::-1] # best to worst
